0a81231072100fcc82c526e250c69e98a322bc7a
Beeeee <beeeee2@outlook.com>
2023-07-17 10:35:33 +0800

    Update ApiDocDemo.java

--- src/main/java/apidoc/ApiDocDemo.java
+++ src/main/java/apidoc/ApiDocDemo.java
@@ -1,13 +1,27 @@
 package apidoc;
 
 
-/**
- * 文档注释
- *
- * @author qwq
- * @version 2.2
- * @see java.lang.String
- * @since JDK1.5
- */
 public class ApiDocDemo {
+
+    /**
+     * 文档注释
+     *
+
+     * @see java.lang.String
+     * @since JDK1.5
+     */
+
+
+    public static final String INFO = "hello";
+
+    /**
+     * 问候语
+     * @param name 给定用户名
+     * @return 返回
+     */
+
+    public String sayHello(String name) {
+        return "hello" + name;
+    }
+
 }
