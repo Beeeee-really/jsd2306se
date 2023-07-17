0fdbf856f62525619d0b6922b1f7c899fe95674f
Beeeee <beeeee2@outlook.com>
2023-07-17 17:11:50 +0800

    Changes

---
+++ src/main/java/string/ToUpperCaseDemo.java
@@ -0,0 +1,14 @@
+package string;
+
+/**
+ * String toUpperCase()
+ * <p>将当前字符串中的英文部分全部转换成大写
+ * <p></p>String toLowerCase()
+ * <p>将当前字符串中的英文部分转换为小写
+ */
+public class ToUpperCaseDemo {
+    public static void main(String[] args) {
+        System.out.println("q w q w q w q wQW QW QW".toUpperCase());
+        System.out.println("qwq wqw qQWQ WQ WQ".toLowerCase());
+    }
+}
