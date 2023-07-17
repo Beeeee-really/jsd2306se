a2a3a90e3dc1c57afc9f4057a8e00cfb64dec7eb
Beeeee <beeeee2@outlook.com>
2023-07-17 17:07:29 +0800

    Create StartWithDemo.java

---
+++ src/main/java/string/StartWithDemo.java
@@ -0,0 +1,14 @@
+package string;
+
+/**
+ * boolean startsWith(String str)
+ * <p>判断当前字符串是否以str开头
+ * <p></p>boolean endsWith(String str)
+ * <p>判断当前字符串是否以str结尾
+ */
+public class StartWithDemo {
+    public static void main(String[] args) {
+        System.out.println("qwq qwq qwq qwq".startsWith("qwq"));
+        System.out.println("qwq qwq qwq qwq".endsWith("awa"));
+    }
+}
