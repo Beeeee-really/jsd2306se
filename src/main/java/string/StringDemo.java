e0cb71c6a293e44b92a6ad0fdc0ed28414c59d48
Beeeee <beeeee2@outlook.com>
2023-07-17 11:54:21 +0800

    Changes

--- src/main/java/string/StringDemo.java
+++ src/main/java/string/StringDemo.java
@@ -2,7 +2,11 @@ package string;
 
 public class StringDemo {
     public static void main(String[] args) {
+        String s1 = "123abc";
+        String s2 = "123abc";
+        String s3 = "123abc";
 
+        System.out.println(s1 == s2);
     }
 
 }
