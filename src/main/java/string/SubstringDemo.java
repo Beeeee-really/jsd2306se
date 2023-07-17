5cd5516be7edc0168e75f4246096bf593fe50e30
Beeeee <beeeee2@outlook.com>
2023-07-17 15:44:25 +0800

    push

--- src/main/java/string/SubstringDemo.java
+++ src/main/java/string/SubstringDemo.java
@@ -2,8 +2,8 @@ package string;
 
 /**
  * String substring(int start,int end);
- * 截取当前字符串中自定范围内的字符串
- * 注：在java API中，当时用两个数字表示范围时，通常表示含头不含尾
+ * <p>截取当前字符串中自定范围内的字符串
+ * <p>注：在java API中，当时用两个数字表示范围时，通常表示含头不含尾
  */
 public class SubstringDemo {
     public static void main(String[] args) {
---
+++ src/main/java/string/Test.java
@@ -0,0 +1,20 @@
+package string;
+
+/**
+ * 课堂练习
+ * <p>实现方法：完成截取网址中的域名
+ */
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "https://www.tedu.cn";
+        String s2 = "www.example.gg";
+        System.out.println(getHostName(s1));
+        System.out.println(getHostName(s2));
+    }
+
+
+    public static String getHostName(String str) {
+
+        return str.substring(str.indexOf(".") + 1, str.lastIndexOf("."));
+    }
+}
