730094d05a063bd9b594a95f3671c8dc4f8de9df
Beeeee <beeeee2@outlook.com>
2023-07-17 12:00:07 +0800

    Update StringDemo.java

--- src/main/java/string/StringDemo.java
+++ src/main/java/string/StringDemo.java
@@ -6,7 +6,14 @@ public class StringDemo {
         String s2 = "123abc";
         String s3 = "123abc";
 
-        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println("qwq");
+        /**
+         * ==  只比较地址
+         *
+         * @author Bee
+         */
+        System.out.println(s2.equals(s1));
     }
 
 }
