1c3956ba63696ffb82a4be819cf9631a44c4ecb5
Beeeee <beeeee2@outlook.com>
2023-07-17 14:42:51 +0800

    Create LengthDemo.java

---
+++ src/main/java/string/LengthDemo.java
@@ -0,0 +1,13 @@
+package string;
+
+/**
+ * int length()
+ * 返回当前字符串的长度(字符个数)
+ */
+public class LengthDemo {
+    public static void main(String[] args) {
+        String str = "我爱java!";
+        int len = str.length();
+        System.out.println(len);//7
+    }
+}
