b722cd93691637507bc7d8161922003323765775
Beeeee <beeeee2@outlook.com>
2023-07-17 16:25:14 +0800

    Create TrimDemo.java

---
+++ src/main/java/string/TrimDemo.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * String trim();
+ * <p>去除字符串两侧空白字符
+ */
+public class TrimDemo {
+    public static void main(String[] args) {
+        System.out.println("   wqw   ".trim());
+    }
+}
