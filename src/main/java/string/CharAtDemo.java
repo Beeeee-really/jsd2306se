2418072560e31607adf6e127fbd44d8ee688b940
Beeeee <beeeee2@outlook.com>
2023-07-17 16:34:02 +0800

    Changes

---
+++ src/main/java/string/CharAtDemo.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * "".charAt();
+ * <p>返回指定字符串的指定字符
+ */
+public class CharAtDemo {
+    public static void main(String[] args) {
+        System.out.println("qwq wq wq".charAt(2));
+    }
+}
