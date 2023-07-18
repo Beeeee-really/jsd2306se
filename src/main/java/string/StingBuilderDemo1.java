04cdc68f89371dceee8ab4011b1c4fd8c3ee7be6
Beeeee <beeeee2@outlook.com>
2023-07-18 09:44:00 +0800

    Update StingBuilderDemo1.java

--- src/main/java/string/StingBuilderDemo1.java
+++ src/main/java/string/StingBuilderDemo1.java
@@ -2,6 +2,8 @@ package string;
 
 /**
  * 由于字符串不适合修改，因此java提供了一个专门用于修改字符串的工具类:StringBuilder
+ * <p>
+ * @author Beeeee
  */
 public class StingBuilderDemo1 {
     public static void main(String[] args) {
@@ -12,5 +14,17 @@ public class StingBuilderDemo1 {
         builder = new StringBuilder("hello world!");
         len = builder.length();
         System.out.println(len);
+
+        /*
+            StringBuilder append(String str);
+            将给定的内容加到字符串末尾，相当于"+"操作
+
+            提供了若干重载，任何java类型都可以作为参数加入，用于与当前字符串连接
+         */
+        builder.append("hello java!");
+        len = builder.length();
+        System.out.println(len);
+        String line = builder.toString();//将当前StringBuilder的内容以String形式返回
+        System.out.println(line);
     }
 }
