0c7253762fc1492d6545ff0ce01d642f17873454
Beeeee <beeeee2@outlook.com>
2023-07-18 15:32:15 +0800

    Create and update

---
+++ src/main/java/object/Demo.java
@@ -0,0 +1,14 @@
+package object;
+
+/**
+ * java中
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Point p = new Point(1,2);
+
+        System.out.println(p);
+    }
+
+}
+
---
+++ src/main/java/object/Point.java
@@ -0,0 +1,16 @@
+package object;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }//自动生成Alt+insert
+}
+
+
+
