3571e8260d4285ce6ab068ed076974c86134a02d
Beeeee <beeeee2@outlook.com>
2023-07-18 14:29:42 +0800

    Update ReplaceAllDemo.java

--- src/main/java/string/ReplaceAllDemo.java
+++ src/main/java/string/ReplaceAllDemo.java
@@ -11,10 +11,19 @@ package string;
 public class ReplaceAllDemo {
     public static void main(String[] args) {
         String line = "abc123def456ghi";
+
+
         /*
             将当前字符串line中的数字部分替换为#NUMBER#
          */
         String str = line.replaceAll("[0-9]+", "#NUMBER#");
+        System.out.println(str);
+
+
+        /*
+            将当前字符串line中的字母部分替换为#WORD#
+         */
+        str = line.replaceAll("[a-zA-Z]+", "#WORD#");
         System.out.println(str);
     }
 }
