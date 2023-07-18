26df6f33285be66d03c55bc75cd7dd55ebaa6c70
Beeeee <beeeee2@outlook.com>
2023-07-18 09:15:59 +0800

    pushs

---
+++ src/main/java/string/StringDemo2.java
@@ -0,0 +1,16 @@
+package string;
+
+/**
+ * String 是不变对象，修改内容时会创建新对象
+ * 因此String不适合频繁修改，性能低，开销大
+ */
+public class StringDemo2 {
+    public static void main(String[] args) {
+        String str = "a";
+        for (int i = 1; i < 999999999; i += 1) {
+            str = str + "a";//超时
+
+        }
+        System.out.println("Oh");
+    }
+}
