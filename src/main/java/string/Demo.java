a246de33417d7f737b4bd48cf83710b0df72e55b
Beeeee <beeeee2@outlook.com>
2023-07-18 14:55:37 +0800

    Create Demo.java

---
+++ src/main/java/string/Demo.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Demo {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        
+    }
+    public static void sayHi(){
+
+    }
+}
