a0ee977e05dff17803f3f7f75edfb4b947407072
Beeeee <beeeee2@outlook.com>
2023-07-18 16:51:53 +0800

    Create and update

---
+++ src/main/java/integer/IntegerDemo1.java
@@ -0,0 +1,37 @@
+package integer;
+
+/**
+ * java为8个基本类型提供了对应的8个包装类
+ * <p>包装类的出现是让基本类型以对象的形式呈现，可以参与面向对象开发
+ * <p>
+ * <p>byte       Byte
+ * <p>short      Short
+ * <p>int        Int
+ * <p>long       Long
+ * <p>float      Float
+ * <p>double     Double
+ * <p>boolean    Boolean
+ * <p>char       Character
+ *
+ * @author Beeeee
+ */
+public class IntegerDemo1 {
+    public static void main(String[] args) {
+        int num = 123;
+//        Integer i1 = num;
+//        System.out.println("i1:" + i1);
+//        Integer i2 = num;
+//        System.out.println("i2:" + i2);
+
+        /*
+            java推荐我们将基本类型转换为对应包装类是使用包装类的静态方法valueOf()
+
+            对于Integer的valueOf方法总是缓存-128-127之间数字对象进行重用
+         */
+        Integer i1 = Integer.valueOf(num);
+        Integer i2 = Integer.valueOf(num);
+
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(num));
+    }
+}
