ea92c12b07faee0201c1bd4d7c07f8608c0b6eb9
Beeeee <beeeee2@outlook.com>
2023-07-19 14:51:47 +0800

    Update IteratorDemo.java

--- src/main/java/collection/NewForDemo.java
+++ src/main/java/collection/NewForDemo.java
@@ -1,5 +1,10 @@
 package collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  * 语法：
  */
@@ -11,8 +16,38 @@ public class NewForDemo {
             System.out.println(e);
         }
 
+        /*
+            新循环会在运行时变为循环
+         */
         for (String e : array) {
             System.out.println(e);
+        }
+
+
+        Collection c = new ArrayList();
+        c.add("one");
+        c.add("two");
+        c.add("three");
+        c.add("four");
+        c.add("five");
+//        c.add(123);//编译器认为这里add方法参数类型应当是String
+
+        for (Object o : c) {
+            String e = (String) o;
+            System.out.println(e);
+        }
+/*
+                public interface Iterator<E> {
+                    boolean hasNext();
+                    E next();
+                }
+                迭代器也支持泛型，指定时与其他遍历的集合
+ */
+        Iterator it = c.iterator();
+        while (it.hasNext()) {
+            String e = (String) it.next();
+            System.out.println(e);
+
         }
     }
 }
