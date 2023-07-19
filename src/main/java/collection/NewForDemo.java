8eebb9077dd7c1d298f59f251f406443a7e46c53
Beeeee <beeeee2@outlook.com>
2023-07-19 14:23:39 +0800

    Update NewForDemo.java

--- src/main/java/collection/NewForDemo.java
+++ src/main/java/collection/NewForDemo.java
@@ -1,7 +1,18 @@
 package collection;
 
 /**
- *
+ * 语法：
  */
 public class NewForDemo {
+    public static void main(String[] args) {
+        String[] array = {"one", "two", "three", "four", "five"};
+        for (int i = 0; i < array.length; i += 1) {
+            String e = array[i];
+            System.out.println(e);
+        }
+
+        for (String e : array) {
+            System.out.println(e);
+        }
+    }
 }
