3ba515e442a2dfb91ae43aa3778f1aee3ddd888c
Beeeee <beeeee2@outlook.com>
2023-07-20 17:15:47 +0800

    Update MkDirDemo.java

--- src/main/java/file/MkDirDemo.java
+++ src/main/java/file/MkDirDemo.java
@@ -11,9 +11,13 @@ import java.io.File;
 public class MkDirDemo {
     public static void main(String[] args) {
         //在当前目录下新建一个目录:demo
-        File file = new File("./demo");
+        File file = new File("./demo/qwq/awa/qq");
         if (!(file.exists())) {
-            file.mkdir();
+            /*
+                mkdir()方法创建目录时要求该目录所在的目录必须存在！否则创建失败
+             */
+//            file.mkdir();
+            file.mkdirs();//创建多个目录
             System.out.println("目录已创建");
         } else {
             System.out.println("目录已存在");
