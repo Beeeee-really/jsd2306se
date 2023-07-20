76df44ea15d7357341707dd3d6d9b98fb7567623
Beeeee <beeeee2@outlook.com>
2023-07-20 13:09:13 +0800

    Update SortListDemo.java

--- src/main/java/collection/SortListDemo.java
+++ src/main/java/collection/SortListDemo.java
@@ -20,7 +20,22 @@ public class SortListDemo {
         }
         System.out.println(list);
 
-        Collections.sort(list);
+//        Collections.sort(list);
+
+        //定义比较器
+        Comparator<Object> c = new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+
+
+            public int compare(int o1, int o2) {
+                return o2 - o1;
+            }
+        };
+        Collections.sort(list, c);
+
 
         System.out.println(list);
     }
