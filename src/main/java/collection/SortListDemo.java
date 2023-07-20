75174505d04661abb426a7dd25a0367ea7f1789c
Beeeee <beeeee2@outlook.com>
2023-07-20 10:24:45 +0800

    Update

---
+++ src/main/java/collection/SortListDemo.java
@@ -0,0 +1,27 @@
+package collection;
+
+import java.util.*;
+
+/**
+ * 集合的排序
+ * <p></p>集合的工具类:java.util.Collection提供了一个静态方法sort,可以对List集合排序
+ * <p>自然排序
+ * <p>
+ *
+ * @author Beeeee
+ */
+public class SortListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i += 1) {
+            list.add(random.nextInt(100));
+        }
+        System.out.println(list);
+
+        Collections.sort(list);
+
+        System.out.println(list);
+    }
+}
---
+++ src/main/java/collection/SortListDemo2.java
@@ -0,0 +1,36 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 演示排序自定义类型的元素
+ */
+public class SortListDemo2 {
+    public static void main(String[] args) {
+        List<Point> list = new ArrayList<>();
+        list.add(new Point(1,2));
+        list.add(new Point(3,4));
+        list.add(new Point(5,6));
+        list.add(new Point(7,8));
+        list.add(new Point(9,10));
+        System.out.println(list);
+        /*
+            compare:比较
+
+            Collections.sort(List list)
+            要求集合元素必须实现Comparable接口，否则编译不通过
+
+            如果是排序自定义元素时，并不推荐为了排序的目的实现该接口，因为这个sort此番操作对我们的代码已经产生了侵入性
+
+            侵入性：当我们使用某个组件功能时，他要求我们为其修改我们的代码来满足其运行要求
+            此时此操作就称为侵入性
+            侵入性不利于程序的维护和扩展，实际开发中应当尽力避免
+         */
+        Collections.sort(list);
+
+        System.out.println(list);
+    }
+}
