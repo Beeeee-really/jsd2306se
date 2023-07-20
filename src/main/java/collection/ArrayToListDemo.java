68fd5123aa5ce25e96798f284cd4f6111d28bf85
Beeeee <beeeee2@outlook.com>
2023-07-20 09:39:59 +0800

    Update ArrayToListDemo.java

--- src/main/java/collection/ArrayToListDemo.java
+++ src/main/java/collection/ArrayToListDemo.java
@@ -16,5 +16,18 @@ public class ArrayToListDemo {
         List<String> list = Arrays.asList(arrays);
         System.out.println(list.size());
         System.out.println("list:" + list);
+        System.out.println(Arrays.toString(arrays));
+
+        /*
+            由于数组是定长的,因此该集合不能做增删等操作
+            会抛出异常:UnsupportedOperationException
+                        不支持         操作
+         */
+
+        list.set(1, "six");
+        System.out.println("list:" + list);
+        System.out.println(Arrays.toString(arrays));
+
+//        list.add("qwq");
     }
 }
