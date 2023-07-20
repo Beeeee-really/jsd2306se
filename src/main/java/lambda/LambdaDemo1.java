2599b689f9af27cfd0d967a35b312e914bc13b4e
Beeeee <beeeee2@outlook.com>
2023-07-20 14:23:27 +0800

    Update LambdaDemo1.java

--- src/main/java/lambda/LambdaDemo1.java
+++ src/main/java/lambda/LambdaDemo1.java
@@ -16,8 +16,20 @@ public class LambdaDemo1 {
             }
         };
         //lambda表达式写法,方法中参数的类型可以忽略不写
-        Comparator<String> c2 = (String o1,String o2)->{
-            return o1.length()-o2.length();
+        Comparator<String> c2 = (String o1, String o2) -> {
+            return o1.length() - o2.length();
         };
+
+        Comparator<String> c3 = (o1, o2) -> {
+            return 0;
+        };
+
+        Comparator<String> c4 = (o1, o2) -> o1.length() - o2.length();//适用于一句代码
+//        Comparator<String> c4 = new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.length() - o2.length();
+//            }
+//        }
     }
 }
