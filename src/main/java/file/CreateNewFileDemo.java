9b5dde6612df0ad48cc24faf2e8323f85d9e8b7b
Beeeee <beeeee2@outlook.com>
2023-07-20 16:36:24 +0800

    Update CreateNewFileDemo.java

--- src/main/java/file/CreateNewFileDemo.java
+++ src/main/java/file/CreateNewFileDemo.java
@@ -1,7 +1,19 @@
 package file;
 
+import java.io.File;
+
 public class CreateNewFileDemo {
     public static void main(String[] args) {
-        
+        //在当前项目目录下创建一个文件
+        File file = new File("./test.txt");
+        /*
+            boolean exists()
+            如果当前file表示的文件和目录已经存在则返回true，否则返回false
+         */
+        if (file.exists()){
+            System.out.println("该文件已存在");
+        }else{
+            file.createNewFile();
+        }
     }
 }
