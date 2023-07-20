278c3e1a5e370a4d08e8d6f49071cd9a10650992
Beeeee <beeeee2@outlook.com>
2023-07-20 16:51:30 +0800

    emmm

--- src/main/java/file/CreateNewFileDemo.java
+++ src/main/java/file/CreateNewFileDemo.java
@@ -11,11 +11,15 @@ public class CreateNewFileDemo {
             boolean exists()
             如果当前file表示的文件和目录已经存在则返回true，否则返回false
          */
-        if (file.exists()){
-            System.out.println("该文件已存在");
-        }else{
-            file.createNewFile();
-            System.out.println("文件已创建");
+//        if (file.exists()){
+//            System.out.println("该文件已存在");
+//        }else{
+//            file.createNewFile();
+//            System.out.println("文件已创建");
+//        }
+        for (int i = 1; i <= 100; i += 1) {
+            File file2 = new File("./" + i + ".txt");
+            file2.createNewFile();
         }
     }
 }
