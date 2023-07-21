ba7b78a41bf63e2de2bebd81b24524d5b8042f54
Beeeee <beeeee2@outlook.com>
2023-07-21 14:30:47 +0800

    0

---
+++ Demo.py
@@ -0,0 +1,13 @@
+import random
+while True:
+    i = input("hi!")
+    if i == "hi!" :
+        print("qwq")
+    elif i == "qwq":
+        print(":))))))")
+    elif i == "Random":
+        i1 = input("i1:")
+        i2 = input("i2:")
+        '''print(random.randint(i1,i2))'''
+    else:
+        print("awa")
---
+++ src/main/java/io/FISDemo.java
@@ -0,0 +1,28 @@
+package io;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * 使用文件输入流从文件读取字节
+ */
+public class FISDemo {
+    public static void main(String[] args) throws IOException {
+        /*
+            将当前文件目录下的文件中的字节读取出来
+         */
+        FileInputStream fis = new FileInputStream("fos.dat");
+        int d = fis.read();
+        System.out.println(d);
+
+        d = fis.read();
+        System.out.println(d);
+
+        d = fis.read();
+        System.out.println(d);
+
+
+    }
+}
