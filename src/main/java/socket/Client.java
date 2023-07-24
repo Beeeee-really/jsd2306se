2fc195cec7b0f81444c564021eb56f913b7a1eeb
Beeeee <beeeee2@outlook.com>
2023-07-24 16:59:38 +0800

    Update Client.java

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,4 +1,21 @@
 package socket;
 
+/**
+ * 聊天室客户端
+ */
 public class Client {
+
+    public Client(){
+
+    }
+
+    public void start(){
+        
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
 }
