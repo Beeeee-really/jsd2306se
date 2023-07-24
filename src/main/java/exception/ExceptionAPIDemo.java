1573ab0a322ceff3cb2a61e8061bc86f43bc3806
Beeeee <beeeee2@outlook.com>
2023-07-24 15:35:42 +0800

    Create ExceptionAPIDemo.java

---
+++ src/main/java/exception/ExceptionAPIDemo.java
@@ -0,0 +1,19 @@
+package exception;
+
+/**
+ * 异常中的常用的用法
+ */
+public class ExceptionAPIDemo {
+    public static void main(String[] args) {
+        System.out.println("程序开始了");
+        try {
+            String name = "abc";
+            System.out.println(Integer.parseInt(name));
+        } catch (NumberFormatException e) {
+            System.out.println("出错了");
+            e.printStackTrace();
+        }
+        System.out.println("程序结束了");
+        System.err.println("程序结束了");
+    }
+}
