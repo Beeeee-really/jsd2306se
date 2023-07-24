3174c0655171f04700ff15e403e262e114dc5145
Beeeee <beeeee2@outlook.com>
2023-07-24 15:25:35 +0800

    .

--- src/main/java/exception/Person.java
+++ src/main/java/exception/Person.java
@@ -6,7 +6,7 @@ package exception;
 public class Person {
     public int age;
 
-    public void setAge(int age) {
+    public void setAge(int age) throws Exception {
         if (age <= 0 || age > 100) {
             throw new RuntimeException("年龄超出了范围!");
         }
--- src/main/java/exception/ThrowDemo.java
+++ src/main/java/exception/ThrowDemo.java
@@ -4,7 +4,7 @@ package exception;
  * throw关键字，可以主动对外抛出一个异常
  */
 public class ThrowDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Person p = new Person();
         p.setAge(1000000);
         System.out.println("此人年龄:" + p.getAge());
