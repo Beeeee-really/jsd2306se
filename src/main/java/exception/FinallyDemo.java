b8634ea08dee72a143d51ab4dc0633232ebaa290
Beeeee <beeeee2@outlook.com>
2023-07-24 10:54:15 +0800

    .

--- src/main/java/exception/FinallyDemo.java
+++ src/main/java/exception/FinallyDemo.java
@@ -11,12 +11,16 @@ public class FinallyDemo {
         System.out.println("程序开始了");
         try {
             String line = null;
+//            String line = "abc";
             System.out.println(line.length());
+            return;
         } catch (Exception e) {
             System.out.println("出错了");
         } finally {
             System.out.println("finally中的代码执行了");
         }
+
+
         System.out.println("程序结束了");
     }
 }
---
+++ src/main/java/exception/FinallyDemo2.java
@@ -0,0 +1,30 @@
+package exception;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 异常处理机制在IO中的应用
+ */
+public class FinallyDemo2 {
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("fos.dat");
+            fos.write(1);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
