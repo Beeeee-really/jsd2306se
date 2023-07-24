12380dc32aca068dedae263869f4e06f4a7372df
Beeeee <beeeee2@outlook.com>
2023-07-24 17:48:25 +0800

    .

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,14 +1,35 @@
 package socket;
 
+import java.io.IOException;
 import java.net.Socket;
 
 /**
  * 聊天室客户端
  */
 public class Client {
+    /*
+        java.net.Socket 套接字,英文原译:插座
+
+        Socket封装了TCP协议的通讯细节，使用它就可以
+     */
     private Socket socket;
 
     public Client(){
+        /*
+            Socket常用的构造器
+            Socket(String host,int port)
+            该构造器实例化时就是根据指定的路径与服务端链接的过程
+            参数1:服务区所在的IP地址
+            参数2:运行在服务器上对应的服务器端应用程序的端口号
+            IP地址用于找到网络上服务器所在的计算机，端口号用于找到运行在计算机上的应用程序
+         */
+        try {
+            System.out.println("正在连接服务端...");
+            socket = new Socket("176.46.103.238",8088);
+            System.out.println("与服务端建立连接!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -1,4 +1,43 @@
 package socket;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 聊天室服务端
+ */
 public class Server {
+    /*
+        java.net.ServerSocket
+        ServerSocket是运行在服务端的，主要有两个作用
+        1:向系统申请并打开服务端口，客户端可以通过这个端口与服务器建立连接
+        2:监听该接口，一旦客户端请求连接就会接受连接同时创建一个Socket对象使用它就可以与客户端交互了
+     */
+    private ServerSocket serverSocket;
+
+    public Server() {
+        try {
+            System.out.println("服务器启动中...");
+            serverSocket = new ServerSocket(8088);
+            System.out.println("服务器启动成功!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start() {
+        try {
+            System.out.println("等待客户端连接...");
+            Socket socket = serverSocket.accept();//阻塞
+            System.out.println("一个客户端连接了!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
 }
