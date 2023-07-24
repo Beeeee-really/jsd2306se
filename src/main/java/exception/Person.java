4356c659b95477b1d2e1351f08f8979ae4876261
Beeeee <beeeee2@outlook.com>
2023-07-24 14:15:27 +0800

    .

---
+++ src/main/java/exception/Person.java
@@ -0,0 +1,16 @@
+package exception;
+
+/**
+ * 使用当前类测试异常地抛出
+ */
+public class Person {
+    public int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/exception/ThrowDemo.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * throw关键字，可以主动对外抛出一个异常
+ */
+public class ThrowDemo {
+    public static void main(String[] args) {
+
+    }
+}
