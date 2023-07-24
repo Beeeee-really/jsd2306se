720b96146139155416819240bfbfba9eab2b8dd1
Beeeee <beeeee2@outlook.com>
2023-07-24 15:47:29 +0800

    Update IllegalAgeException.java

--- src/main/java/exception/IllegalAgeException.java
+++ src/main/java/exception/IllegalAgeException.java
@@ -5,5 +5,22 @@ package exception;
  * <p>自定义异常通常用来表达那些不满足业务场景的问题上
  */
 public class IllegalAgeException extends Exception{
-    
+    public IllegalAgeException() {
+    }
+
+    public IllegalAgeException(String message) {
+        super(message);
+    }
+
+    public IllegalAgeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalAgeException(Throwable cause) {
+        super(cause);
+    }
+
+    public IllegalAgeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
