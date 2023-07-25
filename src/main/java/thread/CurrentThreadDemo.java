5325d969a1b1d054fa59682cb0d3b990700eaa2e
Beeeee <beeeee2@outlook.com>
2023-07-25 15:23:47 +0800

    Create CurrentThreadDemo.java

---
+++ src/main/java/thread/CurrentThreadDemo.java
@@ -0,0 +1,14 @@
+package thread;
+
+/**
+ * java中所有的代码都是靠线程运行的
+ * 线程Thread提供了一个静态方法
+ * static Thread currentThread()
+ * 该方法会返回一个运行这个方法的线程(当前线程)
+ */
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+        Thread main = Thread.currentThread();//获取运行main方法的主线程
+        System.out.println(main);
+    }
+}
