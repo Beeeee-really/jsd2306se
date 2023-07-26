0e0625aa235026a932e236a89ffd7d26542be29c
Beeeee <beeeee2@outlook.com>
2023-07-26 10:02:11 +0800

    Update SleepDemo.java

--- src/main/java/thread/SleepDemo.java
+++ src/main/java/thread/SleepDemo.java
@@ -1,6 +1,8 @@
 package thread;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 睡眠阻塞
@@ -21,18 +23,46 @@ public class SleepDemo {
 //        System.out.println("程序结束了");
 
 
-        System.out.println("输入数字:");
-        Scanner scan = new Scanner(System.in);
-        int time = scan.nextInt();
-        for (int i = time; i >= 0; i -= 1) {
-            System.out.println(i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(
+                        System.in
+                )
+        );
+        try {
+            System.out.println("输入数字:");
+            String line = bufferedReader.readLine();
+            int time = Integer.parseInt(line);
+            System.out.println(time);
+            System.out.println("倒计时开始!");
+            for (int i = time; i >= 0; i -= 1) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+
+//        System.out.println("输入数字:");
+
+//        Scanner scan = new Scanner(System.in);
+//        int time = scan.nextInt();
+//        System.out.println("倒计时开始!");
+//        for (int i = time; i >= 0; i -= 1) {
+//            System.out.println(i);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
         System.out.println("倒计时结束了!");
     }
 }
