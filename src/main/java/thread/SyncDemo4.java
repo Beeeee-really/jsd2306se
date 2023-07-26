d10123603e3185545faa6138bc75e32cd714b3d3
Beeeee <beeeee2@outlook.com>
2023-07-26 17:32:14 +0800

    Create SyncDemo4.java

---
+++ src/main/java/thread/SyncDemo4.java
@@ -0,0 +1,38 @@
+package thread;
+
+/**
+ * 当使用多个...
+ */
+public class SyncDemo4 {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        Thread t1 = new Thread(foo::methodA);
+        Thread t2 = new Thread(foo::methodB);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Foo {
+    public synchronized void methodA() {
+        Thread t = Thread.currentThread();
+        System.out.println(t.getName() + ":正在执行A方法");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public synchronized void methodB() {
+        Thread t = Thread.currentThread();
+        System.out.println(t.getName() + ":正在执行B方法");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
