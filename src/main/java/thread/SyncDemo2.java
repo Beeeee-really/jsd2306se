0392ae7f8344a6534587fd004d2059ba926561ad
Beeeee <beeeee2@outlook.com>
2023-07-26 15:22:04 +0800

    Create SyncDemo2.java

---
+++ src/main/java/thread/SyncDemo2.java
@@ -0,0 +1,36 @@
+package thread;
+
+/**
+ * 有效地缩小同步范围可以在保证并发安全的前提下尽可能地提高并发性能56同步块7synchronized（同步监视器对象）{8需要多个线程同步执行的代码片段*}
+ */
+public class SyncDemo2 {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+
+        Thread t1 = new Thread() {
+            public void run() {
+
+            }
+        };
+
+
+    }
+}
+
+class Shop {
+    public synchronized void buy() {
+        try {
+            Thread t = Thread.currentThread();//获取buy方法的线程
+            System.out.println(t.getName() + "正在挑衣服");
+            Thread.sleep(5000);
+
+            System.out.println(t.getName() + "正在试衣服");
+            Thread.sleep(5000);
+
+            System.out.println(t.getName() + ":结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
