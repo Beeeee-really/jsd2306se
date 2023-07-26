5acefda70607e9ee4727a3157fb0383272a3a0ba
Beeeee <beeeee2@outlook.com>
2023-07-26 09:47:17 +0800

    Update SleepDemo.java

--- src/main/java/thread/SleepDemo.java
+++ src/main/java/thread/SleepDemo.java
@@ -1,5 +1,7 @@
 package thread;
 
+import java.util.Scanner;
+
 /**
  * 睡眠阻塞
  * <p></p>Thread提供了一个静态方法:
@@ -8,14 +10,29 @@ package thread;
  */
 public class SleepDemo {
     public static void main(String[] args) {
-        System.out.println("程序开始了");
+//        System.out.println("程序开始了");
+//
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        System.out.println("程序结束了");
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        System.out.println("输入数字:");
+        Scanner scan = new Scanner(System.in);
+        int time = scan.nextInt();
+        for (int i = time; i >= 0; i -= 1) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
         }
-
-        System.out.println("程序结束了");
+        System.out.println("倒计时结束了!");
     }
 }
